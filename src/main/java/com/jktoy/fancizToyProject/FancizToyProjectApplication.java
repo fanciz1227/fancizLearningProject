8bb2f979e6f5dd754a49a6e9b3e766b8bae595f0
fanciz1227 <fanciz1227@gmail.com>
2021-08-01 19:12:53 +0900

    fanciz Toy Project - project setting

---
+++ src/main/java/com/jktoy/fancizToyProject/FancizToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jktoy.fancizToyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancizToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancizToyProjectApplication.class, args);
+	}
+
+}
